56ae5b2356ba60fb925e3dbaf956535062df7aaa
Marc-Antoine Parent <maparent@acm.org>
2025-03-23 00:23:54 -0400

    full crud on org

--- processieve/api.py
+++ processieve/api.py
@@ -1,19 +1,57 @@
-from typing import List
+from typing import List, Dict, Any, Union
 
 from pydantic import BaseModel
+from fastapi import HTTPException, status
 
 from .models import Organization
 from .main import api_router
 from . import linkMlDb
 
+
+class NotFound(HTTPException):
+    def __init__(
+        self, detail: Any = None, headers: Dict[str, str] | None = None
+    ) -> None:
+        super().__init__(status.HTTP_404_NOT_FOUND, detail, headers)
+
 def dump(m: BaseModel) -> dict:
     d = m.model_dump()
-    d['category'] = org.schema()['title']
+    d['category'] = m.schema()['title']
     return d
 
-@api_router.get('/organizations')
+
+def clean(m: Union[Dict, List[Dict]]) -> Union[Dict, List[Dict]]:
+    if isinstance(m, list):
+        for r in m:
+            r.pop('category')
+    else:
+        m.pop('category')
+    return m
+
+
+@api_router.get('/organization')
 def get_organizations() -> List[Organization]:
-    rows = linkMlDb.find({"category": "Organization"}).rows
-    for row in rows:
-        row.pop('category', None)
-    return rows
+    res = linkMlDb.find({"category": "Organization"})
+    return clean(res.rows)
+
+
+@api_router.get('/organization/{id}')
+def get_organization(id: str) -> Organization:
+    res = linkMlDb.find(dict(category= "Organization", id=id))
+    if not res.num_rows:
+        raise NotFound()
+    return clean(res.rows[0])
+
+
+
+@api_router.post('/organization')
+def add_organization(org: Organization) -> Organization:
+    res = linkMlDb.store(dump(org))
+    linkMlDb.commit()
+    return res
+
+@api_router.patch('/organization')
+def update_organization(org: Organization) -> Organization:
+    res = linkMlDb.update(dump(org))
+    linkMlDb.commit()
+    return res
